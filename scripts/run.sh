d435d607f00500c76df81d270cac6a38846d3797
rednafi <redowan.nafi@gmail.com>
2020-09-04 18:54:33 +0600

    Updated run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,8 +11,8 @@ docker rm -v $(docker ps -a -q -f status=exited)
 # Remove dangling volumes
 docker volume rm $(docker volume ls -qf dangling=true)
 
-
 # Spin up the new container
+# The if-else and the getopts is to take in the worker argument
 if getopts "n:" arg; then
 docker-compose -f docker-compose.yml up \
                 -d                      \
