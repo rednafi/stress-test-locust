514fc2623e72027d58a9913b500862978736d9ca
rednafi <redowan.nafi@gmail.com>
2020-09-04 18:52:23 +0600

    Updated run script

---
+++ scripts/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+docker-compose down
+
+# Remove dangling images (-q=quiet, -f=without prompt)
+​docker rmi $(docker images -q -f dangling=true)
+
+# Remove all stopped containers
+docker rm -v $(docker ps -a -q -f status=exited)
+
+# Remove dangling volumes
+docker volume rm $(docker volume ls -qf dangling=true)
+
+
+# Spin up the new container
+if getopts "n:" arg; then
+docker-compose -f docker-compose.yml up \
+                -d                      \
+                --build                  \
+                --scale worker=$OPTARG
+else
+docker-compose -f docker-compose.yml up \
+                -d                      \
+                --build                  \
+                --scale worker=1
+fi
