29edc3f6b9727352064df770ac13b9bc3f072937
redowan <redowan.nafi@gmail.com>
2022-03-02 02:19:16 +0600

    Cleanup tests, closes #51

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-
+from src import settings
 
 @patch.dict(
     "src.settings.os.environ",
@@ -7,7 +7,6 @@ from unittest.mock import patch
 )
 @patch("src.settings.load_dotenv", autospec=True)
 def test_envvars(load_dotenv):
-    from src import settings
 
     assert settings.os.environ["HOST"] == "dummy_host"
     assert settings.os.environ["API_TOKEN"] == "dummy_api_token"
